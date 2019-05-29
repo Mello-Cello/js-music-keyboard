22fc09348fb49eb34976f68f49338ab9fbf4c14f
Mello-Cello <celloelle@gmail.com>
2019-05-28 21:15:42 -0700

    Completed click and keydown actions to play each note.

--- noise.js
+++ noise.js
@@ -13,50 +13,86 @@ $(document).ready( function() {
   const playC = () => {
     noteC.currentTime = 0;
     noteC.play()
-  }
+  };
+
+  const playD = () => {
+    noteD.currentTime = 0;
+    noteD.play();
+  };
+  
+  const playE = () => {
+    noteE.currentTime = 0;
+    noteE.play();
+  };
+  
+  const playF = () => {
+    noteF.currentTime = 0;
+    noteF.play();
+  };
+  
+  const playG = () => {
+    noteG.currentTime = 0;
+    noteG.play();
+  };
+  
+  const playA = () => {
+    noteA.currentTime = 0;
+    noteA.play();
+  };
+  
+  const playB = () => {
+    noteB.currentTime = 0;
+    noteB.play();
+  };
+
+
   // const noteD = document.getElementById("dAudio");
   
   
   $('body').keydown((event) => {
     if (event.key === 'c') {
       playC();
+    } else if (event.key === 'd') {
+      playD();
+    } else if (event.key === 'e') {
+      playE();
+    } else if (event.key === 'f') {
+      playF();
+    } else if (event.key === 'g') {
+      playG();
+    } else if (event.key === 'a') {
+      playA();
+    } else if (event.key === 'b') {
+      playB();
     }
   });
-  
-       
+   
   $('.c').click(function() { 
-    noteC.currentTime = 0;
-    noteC.play()
+    playC();
   });
 
   $('.d').click(function() { 
-    noteD.currentTime = 0;
-    noteD.play()
+    playD()
   });
 
   $('.e').click(function() { 
-    noteE.currentTime = 0;
-    noteE.play()
+    playE();
   });
 
   $('.f').click(function() { 
-    noteF.currentTime = 0;
-    noteF.play()
+    playF();
   });
 
   $('.g').click(function() { 
-    noteG.currentTime = 0;
-    noteG.play()
+    playG();
   });
 
   $('.a').click(function() { 
-    noteA.currentTime = 0;
-    noteA.play()
+    playA();
   });
 
   $('.b').click(function() { 
-    noteB.currentTime = 0;
-    noteB.play()
+    playB();
   });
 
   
