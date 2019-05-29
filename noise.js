26d614752c1d1a39279efe7a1d6cc13c53d723c8
Mello-Cello <celloelle@gmail.com>
2019-05-28 21:03:16 -0700

    Implemented click to play a note. Began type a letter to play a note.

--- noise.js
+++ noise.js
@@ -1,3 +1,66 @@
 $(document).ready( function() {
-  // your code here
+  
+// event handler: click an item with a class of 'c', play file in div with class 'cAudio'
+
+  const noteC = $('#cAudio')[0];
+  const noteD = $("#dAudio")[0];
+  const noteE = $("#eAudio")[0];
+  const noteF = $("#fAudio")[0];
+  const noteG = $("#gAudio")[0];
+  const noteA = $("#aAudio")[0];
+  const noteB = $("#bAudio")[0];
+
+  const playC = () => {
+    noteC.currentTime = 0;
+    noteC.play()
+  }
+  // const noteD = document.getElementById("dAudio");
+  
+  
+  $('body').keydown((event) => {
+    if (event.key === 'c') {
+      playC();
+    }
+  });
+  
+       
+  $('.c').click(function() { 
+    noteC.currentTime = 0;
+    noteC.play()
+  });
+
+  $('.d').click(function() { 
+    noteD.currentTime = 0;
+    noteD.play()
+  });
+
+  $('.e').click(function() { 
+    noteE.currentTime = 0;
+    noteE.play()
+  });
+
+  $('.f').click(function() { 
+    noteF.currentTime = 0;
+    noteF.play()
+  });
+
+  $('.g').click(function() { 
+    noteG.currentTime = 0;
+    noteG.play()
+  });
+
+  $('.a').click(function() { 
+    noteA.currentTime = 0;
+    noteA.play()
+  });
+
+  $('.b').click(function() { 
+    noteB.currentTime = 0;
+    noteB.play()
+  });
+
+  
+
+
+
 });
